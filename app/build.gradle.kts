e8b7e07708c192a7cc33e6e409bc89ad137ac339
Siddhesh <siddhesh@gmail.com>
2026-01-13 11:25:02 +0530

    Re-Write Started - Added DiffusionEngine - Implemented DiffusionEngine into the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.dark.tool_neuron"
         minSdk = 31
         targetSdk = 36
-        versionCode = 5
+        versionCode = 7
         versionName = "1.0.0"
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
