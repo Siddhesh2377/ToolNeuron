5012c0dc4a4fba823ec70c5a547cff477c363cb8
Siddhesh <siddhesh@gmail.com>
2026-01-14 04:00:21 +0530

    Re-Write Started - Fixed Issues, Alot - Improve Model Editor - Improved UI & Performance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.dark.tool_neuron"
         minSdk = 31
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.0"
+        versionCode = 8
+        versionName = "1.0.1"
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
         }
