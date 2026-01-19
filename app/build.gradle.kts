5ed7d5be3a893c884c55eedf06dfabfc0d8ae26f
Siddhesh2377 <siddheshsonar2377@gmail.com>
2026-01-20 04:37:29 +0530

    Added :: Web-Plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.dark.tool_neuron"
         minSdk = 31
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.1.4"
+        versionCode = 21
+        versionName = "1.2.0"
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
         }
