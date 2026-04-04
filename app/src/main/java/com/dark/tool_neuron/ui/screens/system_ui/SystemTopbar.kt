8fa64a60d4ff44afa90f189ec8ae552813acc0cf
home <home@aosp.local>
2026-04-04 08:37:44 +0530

    Added Advance-Nav System

--- app/src/main/java/com/dark/tool_neuron/ui/screens/system_ui/SystemTopbar.kt
+++
@@ -1,9 +0,0 @@
-package com.dark.tool_neuron.ui.screens.system_ui
-
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.runtime.Composable
-
-@Composable
-fun SystemBody(innerPaddingValues: PaddingValues) {
-
-}
