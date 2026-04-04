c07ad5bf8bbb31e74b47884b345448b1c5e7f69f
home <home@aosp.local>
2026-04-04 07:19:05 +0530

    Added Release Build

--- app/src/main/java/com/dark/tool_neuron/ui/theme/Theme.kt
+++ app/src/main/java/com/dark/tool_neuron/ui/theme/Theme.kt
@@ -4,15 +4,14 @@ import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialExpressiveTheme
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MotionScheme
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.material3.Typography
 
 private val DarkColorScheme = darkColorScheme()
 
